648af6c5d3c313e2975d63d0a3182cf0a285d465
trett <roman@trett.ru>
2019-02-26 22:42:23 +0300

    added modal dialog

---
+++ src/main/resources/static/src/components/modalDialog.ts
@@ -0,0 +1,38 @@
+import Vue from "vue";
+import Component from 'vue-class-component'
+
+@Component({
+    template: `
+  <transition name="modalDialog">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+              default body
+            </slot>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer">
+              <button class="modal-default-button" @click="$emit('close')">
+                OK
+              </button>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+`
+})
+export default class ModalDialog extends Vue {
+    showModal = false;
+}
--- src/main/resources/static/src/components/news.ts
+++ src/main/resources/static/src/components/news.ts
@@ -1,15 +1,27 @@
 import Vue from "vue";
 import Component from "vue-class-component";
-import {Feed} from "../models/feed";
+import { Feed } from "../models/feed";
+import ModalDialog from "./modalDialog";
 
 @Component({
+    components: {
+        modalDialog: ModalDialog
+    },
     template: `
         <div>
             <button v-on:click="refresh()">Refresh</button>
+            <button v-on:click="showModal = true">Add Channel</button>
+            <modalDialog v-if="showModal" @close="addChannel()">
+                <h3 slot="header">Add channel</h3>
+                <div slot="body">
+                    <p>Put link to rss:</p>
+                    <input v-model="newChannel"/>
+                </div>
+            </modalDialog>
             <div v-for="feed in feeds">
                 <h2>{{ feed.title }}</h2>
                 <li v-for="feedItem in feed.feedItems">
-                    <a href={{feedItem.link}}>{{feedItem.title}}</a>
+                    <a :href="feedItem.link">{{feedItem.title}}</a>
                 </li>
             </div>
         </div>
@@ -17,7 +29,11 @@ import {Feed} from "../models/feed";
 })
 export default class News extends Vue {
 
+    public showModal = false;
+
     feeds: Array<Feed> = [];
+
+    newChannel = "";
 
     async beforeMount(): Promise<void> {
         await this.getNews();
@@ -37,5 +53,10 @@ export default class News extends Vue {
             throw new Error("Ошибка");
         }
         this.feeds = await response.json();
+    }
+
+    private async addChannel() {
+        console.log(this.newChannel);
+        this.showModal = false;
     }
 }
