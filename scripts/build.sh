cfed50496c67382d3a6a217aeaeee7c7153d2730
trett <roman@trett.ru>
2023-04-23 18:23:53 +0200

    [build] move to Java 17

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,3 @@ echo "Building app"
 bazel run //server:image || exit 1
 bazel run //client:image --define SERVER_URL=${SERVER_URL} --define CLIENT_ID=${CLIENT_ID} --define REDIRECT_URI=${REDIRECT_URI} || exit 1
 
-APP_VERSION=$(git rev-parse --short HEAD)
-echo "Buiding image: $APP_VERSION"
-docker tag bazel/client:image $CONTAINER_REGISTRY/bazel/client:$APP_VERSION
-docker push $CONTAINER_REGISTRY/bazel/client:$APP_VERSION
-docker tag bazel/server:image $CONTAINER_REGISTRY/bazel/server:$APP_VERSION
-docker push $CONTAINER_REGISTRY/bazel/server:$APP_VERSION
