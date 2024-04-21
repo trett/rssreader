db1a66c26792e4ca6227498855902b6f04bf6264
trett <roman@trett.ru>
2024-04-21 17:42:15 +0200

    [FRONT]: fix for webfonts

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,12 +19,8 @@ module.exports = (env, argv) => ({
             },
             {
                 test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 100000
-                    }
-                }]
+                type: 'asset/resource',
+                dependency: { not: ['url'] },
             },
         ],
     },
