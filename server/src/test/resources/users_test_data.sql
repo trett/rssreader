09cf71b082738c96a2d402cc024f6c8d91d0d029
trett <roman@trett.ru>
2025-04-21 19:18:58 +0200

    remove java server

--- server/src/test/resources/users_test_data.sql
+++
@@ -1,5 +0,0 @@
--- ALTER SEQUENCE hibernate_sequence RESTART WITH 1;
-INSERT INTO public.users(principal_name, email, settings)
-    VALUES('123', 'example@test.com', '{}');
-INSERT INTO public.users(principal_name, email, settings)
-    VALUES('234', 'example2@test.com', '{"deleteAfter": 7, "hideRead": false}');
