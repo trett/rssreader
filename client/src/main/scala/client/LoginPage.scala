1a2974c12e2483defddda2824fef16909a460ff9
Roman Tretyakov <roman@trett.ru>
2025-08-11 22:16:49 +0200

    login page update (#100)
    * login page update

--- client/src/main/scala/client/LoginPage.scala
+++ client/src/main/scala/client/LoginPage.scala
@@ -4,42 +4,59 @@ import be.doeraene.webcomponents.ui5.Link
 import be.doeraene.webcomponents.ui5.configkeys.IconName
 import client.NetworkUtils.HOST
 import com.raquo.laminar.api.L.*
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
+
+@js.native @JSImport("/images/background.png", JSImport.Default)
+val backgroundImage: String = js.native
 
 object LoginPage {
 
     def render: Element = div(
         div(
-            background := "linear-gradient(to bottom right, #d6dee6, #549a9d)",
+            background := s"url($backgroundImage)",
+            backgroundSize := "cover",
+            backgroundPosition := "center",
+            backgroundRepeat := "no-repeat",
             height := "100vh",
             justifyContent := "center",
             display := "flex",
             alignItems := "center",
-            flexDirection := "column", // Stack all elements vertically
-            h1("RSS Reader", fontSize := "2rem", marginBottom := "1rem"),
-            p(
-                "Please sign up or sign in to continue.",
-                fontSize := "1.2rem",
-                marginBottom := "2rem",
-                textAlign := "center"
-            ),
             div(
+                background := "white",
+                padding := "40px",
+                borderRadius := "10px",
+                boxShadow := "0 12px 24px rgba(0,0,0,0.5)",
+                width := "400px",
                 display := "flex",
-                flexDirection := "column", // Stack buttons vertically
-                alignItems := "center", // Center buttons horizontally
-                gap := "1rem", // Add spacing between buttons
-                Link(
-                    cls("google-button"),
-                    "Sign Up",
-                    _.href := s"$HOST/signup",
-                    typ("button"),
-                    _.icon := IconName.`sys-add`
+                flexDirection := "column",
+                alignItems := "center",
+                h1("RSS Reader", fontSize := "2rem", marginBottom := "1rem"),
+                p(
+                    "Please sign up or sign in to continue.",
+                    fontSize := "1.2rem",
+                    marginBottom := "2rem",
+                    textAlign := "center"
                 ),
-                Link(
-                    cls("google-button"),
-                    "Sign In",
-                    _.href := s"$HOST/signin",
-                    typ("button"),
-                    _.icon := IconName.`sys-enter`
+                div(
+                    display := "flex",
+                    flexDirection := "column",
+                    alignItems := "center",
+                    gap := "1rem",
+                    Link(
+                        cls("google-button", "signup-button"),
+                        "Sign Up",
+                        _.href := s"$HOST/signup",
+                        typ("button"),
+                        _.icon := IconName.`sys-add`
+                    ),
+                    Link(
+                        cls("google-button"),
+                        "Sign In",
+                        _.href := s"$HOST/signin",
+                        typ("button"),
+                        _.icon := IconName.`sys-enter`
+                    )
                 )
             )
         )
